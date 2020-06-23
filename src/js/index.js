db913f3cff85a57c91cac11152762cb6f355eb6d
Karolina030 <60924470+Karolina030@users.noreply.github.com>
2020-06-23 16:29:30 +0200

    internship

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
-/* Here goes your JS code */
+var successMessage = document.querySelector(".success-message");
+var helloMessage = document.getElementById("show-popup-form");
+successMessage.style.display='flex';
+successMessage.style.display='none';
+
+function showPopup(){
+    document.querySelector('.popup').style.visibility = "visible";
+    helloMessage.style.display='none';
+
+}
+
+function closePopup(){
+    document.querySelector('.popup').style.visibility = "hidden";
+    helloMessage.style.display ='flex';
+}
+
+function showSuccessMessage(){
+    if (validation()) {
+        document.querySelector('.popup').style.visibility = "hidden";
+        helloMessage.style.display='none';
+        successMessage.style.display='flex';
+    }
+}
+
+function validation() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var passwReg=  /^[A-Za-z]\w{7,14}$/;
+    if (email === '' || password === '') {
+        alert("Please fill all fields");
+        return false;
+    } else if (!(email).match(emailReg)) {
+        alert("Invalid email");
+	return false;
+    } else if (!(password).match(passwReg)) {
+        alert("Invalid password");
+        return false;
+    } else {
+	return true;
+    }
+}
